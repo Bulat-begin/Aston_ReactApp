918e832607c622dab1caa865c9389dbdd28cd7af
BulatH <hayrullinb@yandex.ru>
2024-03-01 17:08:36 +0300

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import FetchData from './Components/FetchData/FetchData'
-import Header from './Components/Header/Header'
-import './App.css' 
+import FetchData from './Components/FetchData/FetchData';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import SearchBar from './Components/SearchBar/SearchBar';
+import './App.css' ;
 
- function App(props) {
-  return (
-    <div className='App'>
-      <Header/>
-      <FetchData/>
+function App(props) {
+ return (
+   <div className="App">
+     <Header/>
+     <SearchBar/>
+     <FetchData/>
+      <Footer/>
     </div>
   );
 }
